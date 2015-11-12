0d78b7166780db5b630197302e5c4bb22e618765
Oleg Mazhukin <Oleg.Mazhukin@dataart.com>
2015-11-12 18:33:10 +0200

    Fixed action menu

--- app/src/main/java/net/olmaz/picturechecker/MainActivity.java
+++ app/src/main/java/net/olmaz/picturechecker/MainActivity.java
@@ -1,12 +1,17 @@
 package net.olmaz.picturechecker;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.commit451.nativestackblur.NativeStackBlur;
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 
 import net.i2p.android.ext.floatingactionbutton.FloatingActionButton;
 import net.i2p.android.ext.floatingactionbutton.FloatingActionsMenu;
@@ -18,37 +23,37 @@ import butterknife.OnClick;
 
 public class MainActivity extends BaseActivity {
 
-    static final int REQUEST_TAKE_PHOTO = 1;
-
     @Bind(R.id.img_picture_image)
     protected ImageView mPictureImage;
 
-    @Bind(R.id.blur_seek_bar)
-    protected DiscreteSeekBar mBlurSeekBar;
+    @Bind(R.id.btn_mirror_h)
+    protected FloatingActionButton dark;
+    @Bind(R.id.btn_mirror_v)
+    protected FloatingActionButton bright;
+    @Bind(R.id.btn_zoom)
+    protected FloatingActionButton zoomOut;
+    @Bind(R.id.btn_black_and_white)
+    protected FloatingActionButton blackAndWhite;
+    @Bind(R.id.btn_blur)
+    protected FloatingActionButton blur;
+
+    private Bitmap mImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        FloatingActionsMenu fab = (FloatingActionsMenu) findViewById(R.id.multiple_actions);
-
-        final FloatingActionButton dark = (FloatingActionButton) findViewById(R.id.btn_mirror_h);
         dark.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 ImageEffects.mirrorImageHorizontal(mPictureImage);
             }
         });
-
-        final FloatingActionButton bright = (FloatingActionButton) findViewById(R.id.btn_mirror_v);
         bright.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 ImageEffects.mirrorImageVertical(mPictureImage);
             }
         });
-
-        final FloatingActionButton zoomOut = (FloatingActionButton) findViewById(R.id.btn_zoom);
         zoomOut.setOnClickListener(new View.OnClickListener() {
 
             boolean isZoom;
@@ -60,13 +65,11 @@ public class MainActivity extends BaseActivity {
                     ImageEffects.zoomOut(mPictureImage);
                 } else {
                     isZoom = false;
-                    Picasso.with(MainActivity.this).load(R.drawable.test_image).fit().into(mPictureImage);
+                    showImage();
                 }
             }
         });
-
-        final FloatingActionButton zoomIn = (FloatingActionButton) findViewById(R.id.btn_black_and_white);
-        zoomIn.setOnClickListener(new View.OnClickListener() {
+        blackAndWhite.setOnClickListener(new View.OnClickListener() {
 
             boolean isBlackAndWhite;
 
@@ -81,24 +84,14 @@ public class MainActivity extends BaseActivity {
                 }
             }
         });
-
-        mBlurSeekBar.setOnProgressChangeListener(new DiscreteSeekBar.OnProgressChangeListener() {
+        blur.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onProgressChanged(DiscreteSeekBar seekBar, int value, boolean fromUser) {
-
-            }
-
-            @Override
-            public void onStartTrackingTouch(DiscreteSeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(DiscreteSeekBar seekBar) {
-
+            public void onClick(View v) {
+                Bitmap bitmap = ((BitmapDrawable) mPictureImage.getDrawable()).getBitmap();
+                Bitmap bm = NativeStackBlur.process(bitmap, 5);
+                mPictureImage.setImageBitmap(bm);
             }
         });
-
         showImage();
     }
 
@@ -116,20 +109,4 @@ public class MainActivity extends BaseActivity {
         return R.layout.activity_main;
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
